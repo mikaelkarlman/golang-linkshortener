b990780c166c40f9f23aa6a1e593ed8ba0f03155
Mikael Karlman <mikael.karlman@schibsted.com>
2020-12-14 16:01:04 +0100

    Test of functions, added basic redirects and ID handling

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module linkshortener
 
 go 1.15
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+)
--- linkshortener.go
+++ linkshortener.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-    "github.com/joho/godotenv"
-    "github.com/julienschmidt/httprouter"
-    "log"
-    "os"
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
+var links = map[string]string{
+	"troll":  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+	"jebait": "https://www.youtube.com/watch?v=d1YBv2mWll0",
+	"google": "https://google.com",
+}
+
+//Index server generic page
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+//MatchLinkID checks the ID, matches it and redirects if the key exists
+func MatchLinkID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	var linkID string = ps.ByName("linkID")
+
+	if val, ok := links[linkID]; ok {
+
+		http.Redirect(w, r, val, http.StatusMovedPermanently)
+
+	} else {
+
+		fmt.Fprintf(w, "LinkID: "+string(linkID)+" does not exist")
+
+	}
+}
+
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/:linkID", MatchLinkID)
+
+	log.Println(http.ListenAndServe(":8080", router))
 
 }
