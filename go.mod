5b07d096bf00066fbe781477f8d0bd762f8e24ee
Mikael Karlman <mikael.karlman@schibsted.com>
2020-12-15 17:05:31 +0100

    Added database support and favicon handling

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module linkshortener
 go 1.15
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.9.0
 )
--- linkshortener.go
+++ linkshortener.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
 )
 
-var links = map[string]string{
-	"troll":  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-	"jebait": "https://www.youtube.com/watch?v=d1YBv2mWll0",
-	"google": "https://google.com",
+//Index server generic page
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
 }
 
-//Index server generic page
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func faviconHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	http.ServeFile(w, r, "favicon.ico")
 }
 
 //MatchLinkID checks the ID, matches it and redirects if the key exists
@@ -24,19 +26,56 @@ func MatchLinkID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	var linkID string = ps.ByName("linkID")
 
-	if val, ok := links[linkID]; ok {
+	if linkID != "favicon.ico" {
 
-		http.Redirect(w, r, val, http.StatusMovedPermanently)
+		fmt.Println("LinkID called: " + linkID)
+
+		link := GetDatabaseData(linkID)
+
+		if len(link) != 0 {
+
+			http.Redirect(w, r, link, http.StatusMovedPermanently)
+
+		} else {
+
+			fmt.Fprintf(w, "LinkID: "+string(linkID)+" does not exist")
+
+		}
 
 	} else {
 
-		fmt.Fprintf(w, "LinkID: "+string(linkID)+" does not exist")
+		faviconHandler(w, r, ps)
 
 	}
+
+}
+
+//GetDatabaseData takes a linkID and looks it up in the database, if it exists it returns the link
+func GetDatabaseData(linkID string) string {
+	//Postgres
+	connStr := "user=" + os.Getenv("DB_USER") + " dbname=" + os.Getenv("DB_NAME") + " password=" + os.Getenv("DB_PASSWORD") + " host=" + os.Getenv("DB_HOST") + " sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var link string
+	err = db.QueryRow("SELECT link FROM links WHERE linkID = $1 LIMIT 1;", linkID).Scan(&link)
+	fmt.Println(err)
+	fmt.Println(link)
+
+	return link
 }
 
 func main() {
+	//dotenv
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	//HTTPRouter
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/:linkID", MatchLinkID)
